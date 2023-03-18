3c1b35e944339e33dc540ccab5f7a5c7c2e91f18
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-03-18 18:07:18 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './recipes/recipe-list/recipe/recipe.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent,
   ],
   imports: [
     BrowserModule,
