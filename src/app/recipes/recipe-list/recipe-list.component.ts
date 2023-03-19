043aea723d1f3f45481b1c9431fc0629b44d7f70
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-03-19 13:39:35 -0300

    Estructura basica de los templates. Modelos de recetas e ingredientes.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe("receta","test","https://www.orientalmarket.es/recetas/wp-content/uploads/2022/03/receta-laab.jpg"),
+    new Recipe("receta","test","https://www.orientalmarket.es/recetas/wp-content/uploads/2022/03/receta-laab.jpg")
+  ];
 
 }
